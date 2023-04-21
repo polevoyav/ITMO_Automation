02b09cce7eb5a000312af930eb7bac809d4d595b
Polevoy_AV <polevoyav@gmail.com>
2023-04-21 21:48:08 +0300

    1

---
+++ python_trening/task_8_if_elif_else_or_and.py
@@ -0,0 +1,12 @@
+permit_print = False # True
+num = -4
+if num > 0 and permit_print:
+    print('num - polojitelinoe')
+elif not permit_print:
+    print('pechati gapreshena')
+
+num = 101
+if num > 100 or num <-100:
+    print('-')
+else:
+    print('+')
