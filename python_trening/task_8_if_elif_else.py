8be04174ec61475a298a1016015533775a2e0869
Polevoy_AV <polevoyav@gmail.com>
2023-04-21 21:30:05 +0300

    1

---
+++ python_trening/task_8_if_elif_else.py
@@ -0,0 +1,12 @@
+num_float = -50
+
+# Также попробуйте варианты
+# num_float = 0
+# num_float = -5
+
+if num_float >0:
+    print('polojitelinoe chislo')
+elif num_float == 0:
+    print('noli')
+else:
+    print('chislo otricatelinoe')
